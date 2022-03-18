cb102f3c15af18dfc2d35c4d8ad679e8762572eb
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-18 15:15:22 +0530

    got current Time in required format and created events.json to store events

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const express = require("express");
+const { location } = require("express/lib/response");
 
 const app = express();
+
+const events = require("./events.json");
+
+let arr = []
+
+events.map((el)=>{
+    let [currDate, currTime] = el.date_Time.split(" ");
+
+    currDate = currDate.split("-");
+
+    let eventObj = {
+        event_name : el.event_name,
+        date_Time : [currDate, currTime].join(" ")
+    }
+
+    arr.push(eventObj);
+
+})
+
+
+console.log(arr);
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours()+':'+today.getMinutes()
++':'+today.getSeconds()+"."+ today.getMilliseconds()  ;
+
+
+
+
+console.log(date + ' '+ time);
+
 
 
 app.listen(2233, ()=>{
