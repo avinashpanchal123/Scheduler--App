596799da78ebcccf1c03cf533df4dac20a6e5cb7
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-18 18:49:10 +0530

    getting output for one event
    after getting the output need to work on
    starting new interval with the same function

--- app.js
+++ app.js
@@ -5,34 +5,122 @@ const app = express();
 
 const events = require("./events.json");
 
-let arr = []
+let flag = true;
 
-events.map((el)=>{
-    let [currDate, currTime] = el.date_Time.split(" ");
 
-    currDate = currDate.split("-");
 
-    let eventObj = {
-        event_name : el.event_name,
-        date_Time : [currDate, currTime].join(" ")
+
+
+
+let today;
+let currDate;
+let currTime;
+
+let a;
+let b; 
+
+let sec;
+let hrs;
+let month;
+let date;
+let mins
+
+let event_name;
+let timer  = setInterval(() => {
+    getOutput()
+}, 1000);
+
+
+
+function getOutput(){
+    today = new Date();
+
+     if((today.getMonth()+1) < 10){
+         month = "0"+(today.getMonth()+1)
+     }
+     else{
+         month = (today.getMonth()+1)
+     }
+
+     if( today.getDate() < 10){
+         date = "0"+ today.getDate()
+     }
+     else{
+         date = today.getDate()
+     }
+
+    if( today.getHours() < 10){
+        hrs = "0"+today.getHours()
+    }
+    else{
+        hrs  = today.getHours()
     }
 
-    arr.push(eventObj);
+    if( today.getMinutes() < 10){
+        mins = "0"+ today.getMinutes()
+    }
+    else{
+        mins = today.getMinutes()
+    }
 
-})
+    if( today.getSeconds() < 10) {
+        sec = "0" + today.getSeconds()
+    }
+    else{
+        sec = today.getSeconds()
+    }
+
+    currDate = today.getFullYear()+'-'+month+'-'+date;
 
 
-console.log(arr);
+    currTime = hrs+":"+mins+":"+sec;
 
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours()+':'+today.getMinutes()
-+':'+today.getSeconds()+"."+ today.getMilliseconds()  ;
+    a = (currDate+" "+currTime);
+
+   
+
+   for (let i = 0; i < events.length; i++) {
+  
+    b = events[i].date_Time;
+    
+   if( a == b){
+    clearInterval(timer)
+    event_name = events[i].event_name;
+    console.log("I am called");
+     printOutput(event_name);
+   
+   }
+
+   }
+//    console.log(a);
+  
+
+}
 
 
 
 
-console.log(date + ' '+ time);
+
+function printOutput(str){
+    let arr = str.trim().split("");
+    
+    let left = 0; 
+    let right = arr.length-1;
+
+    while( left < right){
+        let temp = arr[left];
+         arr[left]  = arr[right];
+        arr[right] = temp
+        left++;
+        right--;
+    }
+    let output = arr.join("");
+   
+    let outputTimer = setTimeout(() => {
+        console.log(output);
+        timer = setInterval(getOutput, 200)
+    }, ((output.length) * 1000));
+}
 
 
 
